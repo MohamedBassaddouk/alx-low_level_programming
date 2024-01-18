a42201968300ec8a192dd93f52e3c77917b63738
MohamedBassaddouk <windar2023@gmaol.com>
2024-01-18 06:09:13 -0800

    dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp https://github.com/MohamedBassaddouk/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
